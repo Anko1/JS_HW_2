0398325aa60c7ba30c90500e79f8799943cb9b0b
satanna <fortuna.me@ukr.net>
2017-05-22 22:36:21 +0300

    added design

--- js/common.js
+++ js/common.js
@@ -4,11 +4,8 @@ function numberSum() {
         total += i;
     }
     // console.log(total);
+    logger('Сума від 0 до 3 = ' + total);
 }
-numberSum();
-
-
-
 
 function numberSum2() {
     var sum = 0;
@@ -16,76 +13,80 @@ function numberSum2() {
         sum += i;
     }
     // console.log(sum);
+    logger('Сума від 20 до 220 = ' + sum);
 }
-numberSum2();
-
-
-
 
 function evenNumb() {
-    for (var i = 2; i <= 20; i++)
-    {
+    var log = '';
+    for (var i = 2; i <= 20; i++) {
         if ((i % 2) != 0) {
             continue;
         }
-        // console.log(i);
+        log += i + ' ';
     }
-}
-evenNumb();
 
+    logger('Всі парні числа від 1 до 20 з continue = ' + log);
+}
 
 function withoutContinue() {
-var i = 0;
-    for (i=2;i<=20;i+=2) {
-    if ((i % 2) == 0) {
-        // console.log(i);
-    }
-    }
-}
-withoutContinue();
-
-
-
-function whileNumb() {
-    var i = 9;
-    while(i < 29) {
-        i += 2;
-        // console.log(i);
-    }
-}
-whileNumb();
-
-
-function ex1() {
-    var mob = 3;
-    for (var i = 1; i <=15; i++) {
-        // console.log(i);
-        if (i == mob) {
-            // console.log('My b at ' + i);
-            break;
+    var log = '';
+    for (var i = 2; i <= 20; i += 2) {
+        if ((i % 2) == 0) {
+            // console.log(i);
+            log += i + ' ';
         }
     }
+    logger('Всі парні числа від 1 до 20 без continue = ' + log);
 }
-ex1();
 
+function whileNumb() {
+    var log = '';
+    var i = 9;
+    while (i < 29) {
+        i += 2;
+        // console.log(i);
+        log += i + ' ';
+    }
+    logger('Всі непарні числа від 10 до 30 = ' + log);
+}
+function ex1() {
+    var mob = 3;
+    var  log = '';
 
+    for (var i = 1; i <= 15; i++) {
+        // console.log(i);
+
+        if (i == mob) {
+            // console.log('My b at ' + i);
+            log += 'My birthday at ' + i;
+
+            break;
+        }
+
+        log += i + ' ';
+
+    }
+    logger(log);
+}
 
 function ex2() {
     var i = 1;
+    var log = '';
 
     while (i <= 15) {
+
         // console.log(i);
-        if (i == 3){
+        if (i == 3) {
+            log += 'My birthday with while at ' + i;
             break;
         }
-        i += 1;
+        log += i + ' ';
+        i++;
 
     }
+    logger(log);
     // console.log('My b at ' + i);
 }
-ex2();
-
-
 
 function ask() {
     return prompt('Ведіть число більше 100', '');
@@ -93,31 +94,42 @@ function ask() {
 
 function loop() {
     var num = 0;
-    while (num < 100)
-    {
+
+    while (num < 100) {
         num = ask();
     }
     // console.log(num);
+    logger(num);
 }
 
-loop();
+function loop2() {
+    var num = 0;
+    do {
+        num = ask();
+    } while (num < 100);
 
+    logger(num);
+}
 
-// function loop2() {
-//     var i = 0;
-//     do {
-//         return prompt('Ведіть число більше 100', '');
-//     } while (i < 100);
-// }
-// loop2();
 
 function ex10() {
     var str = '';
-    for(var i = 0; i < 8; i++)
-    {
-        str += '#';
-        console.log(str);
+    for (var i = 0; i < 8; i++) {
+        for(var j = 0; j < i; j++)
+        {
+            str += '#';
+
+        }
+        str += '\n';
     }
+
+    logger('<pre>'+ str + '</pre>');
+    // console.log();
 }
 
-ex10();
+function logger(data) {
+    var log = $('<p class="log"></p>');
+    log.html(data);
+
+    $('#console').prepend(log);
+}
